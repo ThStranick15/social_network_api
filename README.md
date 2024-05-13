a25c259a9459ea55c130256189cc0b32db130da7
ThStranick <ThStranick@gmail.com>
2024-05-13 08:27:11 -0400

    finished reaction routes, README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# social_network_api
+# Social Network API
+
+## Author: Thomas Stranick
+
+## Description
+The Social Network API is designed to be the backend for any social network. Through a combination of express, MongoDB, and Mongoose, all the functionality of a base social network can be achieved. Users can be created along with their unique username and email. Thoughts can be created which are essentially posts on the social network site. They contain some form of text and relate to the user who created it. Users can also add reactions to thoughts that also contain text and the username. Users can friend users and store them within their friendslist where your count is displayed. Each of these can also be updated and deleted if needed.
+
+## Screenshots
+
+<image src="assets\Screenshot 2024-05-13 082000.png">
+
+## Installation
+N/A
+
+## Usage
+To use the Social Network API, use Insomnia or another route tester. Here, one can GET either all Users or Thoughts, or only a singular User or Thought. Users, Thoughts, and Reactions can all be created (POST), as well as friends added to another User. Users and Thoughts can be updated (PUT), as well as deleted (DEL) along with Reactions and Friends.
+
+## Credits
+-npm (express, mongoose)  
+-MongoDB
+
+## Deployment
+Link to the repo -  [Tech Blog](https://github.com/ThStranick15/social_network_api)  
+
+## Contributors
+- Thomas Stranick:  https://github.com/ThStranick15
+
+## License
+Please refer to the MIT LICENSE in the repo
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const {User, Thought} = require('../models')
+const { User, Thought } = require('../models')
 
 //GET all users /api/users
-router.get('/users', async (req,res) =>{
+router.get('/users', async (req, res) => {
     try {
         const users = await User.find()
         res.json(users)
@@ -10,9 +10,9 @@ router.get('/users', async (req,res) =>{
         console.log(error)
     }
 })
- 
+
 //GET a user by id
-router.get('/users/:id', async (req,res) =>{
+router.get('/users/:id', async (req, res) => {
     const id = req.params.id
     try {
         const user = await User.findById(id)
@@ -23,7 +23,7 @@ router.get('/users/:id', async (req,res) =>{
 })
 
 //POST a new user
-router.post('/users', async (req,res) =>{
+router.post('/users', async (req, res) => {
     try {
         const newUser = await User.create(req.body)
         res.json(newUser)
@@ -33,10 +33,10 @@ router.post('/users', async (req,res) =>{
 })
 
 //PUT update a user by id
-router.put('/users/:id', async (req,res) =>{
+router.put('/users/:id', async (req, res) => {
     const id = req.params.id
     try {
-        const updatedUser = await User.findByIdAndUpdate(id, req.body, {new: true})
+        const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true })
         res.json(updatedUser)
     } catch (error) {
         console.log(error)
@@ -44,7 +44,7 @@ router.put('/users/:id', async (req,res) =>{
 })
 
 //DELETE a user by id, BONUS remove associated thoughts when deleted
-router.delete('/users/:id', async (req,res) =>{
+router.delete('/users/:id', async (req, res) => {
     const id = req.params.id
     try {
         const deletedUser = await User.findByIdAndDelete(id)
@@ -55,48 +55,123 @@ router.delete('/users/:id', async (req,res) =>{
 })
 
 //POST add a user to user friends list /api/users/:userId/friends/:friendId
-router.post('/users/:userId/friends/:friendId', async (req,res) =>{
-    
+router.post('/users/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const id = req.params.userId
+        const friendId = req.params.friendId
+
+        const user = await User.findById(id)
+        user.friends.push(friendId)
+        await user.save()
+        res.json(user)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //DELETE remove a user from users friendslist /api/users/:userId/friends/:friendId
-router.delete('/users/:userId/friends/:friendId', async (req,res) =>{
-    
+router.delete('/users/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const id = req.params.userId
+        const friendId = req.params.friendId
+
+        const user = await User.findById(id)
+        const i = user.friends.indexOf(friendId)
+        if (i != -1) { user.friends.splice(i, 1) }
+        await user.save()
+        res.json(user)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //GET all thoughts /api/thoughts
-router.get('/thoughts', async (req,res) =>{
-
+router.get('/thoughts', async (req, res) => {
+    try {
+        const thoughts = await Thought.find()
+        res.json(thoughts)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //GET thought by id
-router.get('/thoughts/:id', async (req,res) =>{
-
+router.get('/thoughts/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const thought = await Thought.findById(id)
+        res.json(thought)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //POST thought, push thought id to user thought array
-router.post('/thoughts/:id', async (req,res) =>{
-
+router.post('/thoughts', async (req, res) => {
+    try {
+        const newThought = await Thought.create(req.body)
+        //push thought id to the user that created it
+        const thoughtId = newThought._id
+        const user = await User.findOne({ username: req.body.username })
+        user.thoughts.push(thoughtId)
+        await user.save()
+        res.json(newThought)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //PUT update thought
-router.put('/thoughts/:id', async (req,res) =>{
-
+router.put('/thoughts/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const updatedThought = await Thought.findByIdAndUpdate(id, req.body, { new: true })
+        res.json(updatedThought)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //DELETE thought
-router.delete('/thoughts/:id', async (req,res) =>{
-
+router.delete('/thoughts/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const deletedThought = await Thought.findByIdAndDelete(id)
+        res.json(deletedThought)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //POST reaction stored in thoughts reaction array /api/thoughts/:thoughtId/reactions
-router.post('/thoughts/:thoughtId/reactions', async (req,res) =>{
-
+router.post('/thoughts/:thoughtId/reactions', async (req, res) => {
+    try {
+        const id = req.params.thoughtId
+        const newThought = await Thought.findById(id).populate('reactions')
+        newThought.reactions.push(req.body)
+        newThought.save()
+        res.json(newThought)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //DELETE reaction stored in thoughts reaction array by id
-router.delete('/thoughts/:thoughtId/reactions/:reactionId', async (req,res) =>{
-
+router.delete('/thoughts/:thoughtId/reactions/:reactionId', async (req, res) => {
+    try {
+        const id = req.params.thoughtId
+        const reactionId = req.params.reactionId
+        const thought = await Thought.findById(id).populate('reactions')
+        const i = thought.reactions.forEach(el => {
+            if(el._id == reactionId)
+                return thought.reactions.indexOf(el)
+        })
+        thought.reactions.splice(i,1)
+        await thought.save()
+        res.json(thought)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router
